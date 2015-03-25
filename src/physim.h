6cae0a99225e09eea5101bdf43e250a0869abf61
shintoo <seanprapp@gmail.com>
2015-03-25 00:02:23 -0400

    fleshing out functions, still

--- src/physim.h
+++ src/physim.h
@@ -10,9 +10,9 @@ typedef struct {
 
 fpos_t findstr(FILE *fp, char *str);
 void readval(FILE *fp, char *str, void *var);
-void vectorread(FILE *fp, vector *vect);
+void vectorread(FILE *fp, fpos_t loc, vector *vect);
 void sforces(FILE *fp, const int nforces, force forces[]);
 void swindow(FILE *fp, const vector window[2]);
 void stime(FILE *fp, vector *time);
 void applyforces(const int nforces, const force forces[], vector objects[], const double mass, const vector *time);
-void display(const vector objects[], const vector window[2], const vector *time);
+void writegrap(FILE * fp, const vector objects[], const vector window[2], const vector *time);
