b80308777250e4cda84ccd091f7673c3e26c1a3e
shintoo <seanprapp@gmail.com>
2015-03-25 00:01:38 -0400

    fleshing out functions, still

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
-/* TODO:
-*	- Create an array of objects, one for each frame
-*	- Change forces to the force structure, create array
-*	- read force components and application time from file
-*	- read object mass, and initial position from same file
-*	- Write the display function
-*	- Write apply force function
+/* Physim: 2D vector-force physics simulator
+*	The purpose of this program is to create a
+*	parametric graph in which x(t) and y(t)
+*	are based off of forces applied to an
+*	object with mass, reading the mass,
+*	forces, time range, and window range
+*	from an easily written file.
+* ------------------------------------------------
+* TODO:
+*	- Write the writegraph function
+*	- fix what doesnt work
 */
 
 
@@ -18,25 +22,31 @@ int main(int argc, int *argv[]) {
 		printf("usage: %s <file>", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	FILE *fp;
-	if ((fp = fopen(argv[1], "r")) == NULL) {
+	FILE *fin, *fout;
+	if ((fin = fopen(argv[1], "r")) == NULL) {
 		printf("%s: error opening file %s", argv[0], argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	unsigned double mass;		// mass of object read from fp
-	unsigned int nforces;		// number of forces
-	// nforces = [from file: find line that begins with "forces: ", get the digits until '\n'
-	// into a string, convert to integer
+	char outs[20];
+	double mass;
+	int nforces;			// number of forces
+	readval(fin, "forces: ", &nforces);
+	readval(fin, "mass: ", &mass);
 	force forces[nforces];
 	vector time;			//.x = begin time, .y = end time
 	vector window[2];		// bottom left and top right window values <x,y>
-	stime(fp, &time);		// reads time from file into time structure (begin, end)
+	stime(fp, &time)		// reads time from file into time structure (begin, end)
 	vector object[time.y - time.x]	// declares array of object structures with an amount of members
 					// equal to range of time
-	swindow(fp, window);		// reads window size (botleft, topright) from file into window
-	sforces(fp, nforces, forces[]);	//reads forces from file into array of force structures
-	applyforces(forces[], object[], &time);
-	display(object[], window, &time);
-	fclose(fp);
+	swindow(fin, window)		// reads window size (botleft, topright) from file into window
+	sforces(fin, nforces, forces);	//reads forces from file into array of force structures
+	applyforces(forces, object, &time);
+	strncpy(outs, argv[1]);
+	outs[15] = '\0';
+	strcat(outs, ".phs");
+	fopen(outs, "w");
+	writegraph(fout, object, window, &time);
+	fclose(fin);
+	fclose(fout);
 	return 0;
 }
--- src/physim.c
+++ src/physim.c
@@ -2,42 +2,55 @@
 
 //finds string str in file fp, returns location
 fpos_t findstr(FILE *fp, char *str) {
-	int find = 0;
-	char temp[64];
-	fpos_t strloc;
+	fpos_t strloc = NULL;
+	char c;
+	int nlct = 0, len = 0;
 
-	while ((fgets(temp, 64, fp)) != NULL) {
-		if((strstr(temp, str)) != NULL) {
-			
-
-	
+	while ((c = str[len++]) != '\0');
+	while ((c = getc(fp)) != EOF) {
+		if (c == '\n')
+			nlct++;
+		if (c == str[0]) {
+			strloc = ftell(fp);
+			for (int i = 1; i < len; i++) {
+				if ((c = getc(fp)) == str[i])
+					continue;
+				else {
+					strloc = NULL;
+					break;
+				}
+			}
+			if (i = len - 1 && strloc != NULL)
+				break;
+		}
+	}
+	return strloc;
 }
 //reads either double type for mass or integer type for nforces from file fp
 void readval(FILE *fp, char *str, void *var) {
 	int i = 0;
-	char temp[6], c;
+	char temp[64], c;
 	fpos_t strloc = findstr(fp, str);
-	
+	if (strloc == NULL) {
+		printf("Error: missing argument: %s", str);
+		exit(EXIT_FAILURE);
+	}
 	fsetpos(fp, strloc);
 	while (isdigit(c = getc(fp) || c == '.')
 		 temp[i++] = c;
 	temp[i + 1] = '\0';
-	puts(temp);
-	if (strcmp(str, "mass: "))
+	if (str[0] == 'm')
 		*var = atof(temp);
-	else if (strcmp(str, "forces: "))
+	else if (str[0] == 'f')
 		*var = atoi(temp);
 }
 //reads numeric values on line line in file fp into vector structure vect
-void vectorread(FILE *fp, int line, vector *vect) {
+void vectorread(FILE *fp, fpos_t loc, vector *vect) {
 	char c, lf, val1[32], *val2[32];
 	double x, y;
 	int i = 0, 1negflag, 2negflag;
 
-	while (i < line) {
-		if ((lf = getc(fp)) == '\n')
-			i++;
-	}
+	fsetpos(fp, loc);
 	i = 0;
 	while ((c = getc(fp)) != EOF) {
 		if (c == '<')
@@ -50,9 +63,9 @@ void vectorread(FILE *fp, int line, vector *vect) {
 	if ((c = getc(fp)) == '-')
 		1negflag = 1;
 	else
-		val1[0] = c;
+		val1[i++] = c;
 	while (isdigit(c = getc(fp)) || c == '.')
-		val1[++i] = c;
+		val1[i++] = c;
 	if (c != ',') {
 		puts("Error reading vector: expecting ',' between values.");
 		exit(EXIT_FAILURE);
@@ -62,9 +75,9 @@ void vectorread(FILE *fp, int line, vector *vect) {
 	if ((c = getc(fp)) == '-')
 		2negflag = 1;
 	else
-		val2[i] = c;
+		val2[i++] = c;
 	while (isdigit(c = getc(fp)) || c == '.')
-		val2[++i] = c;
+		val2[i++] = c;
 	if (c != '>') {
 		puts("Error reading vector: expecting '>'. use format <x,y>");
 		exit(EXIT_FAILURE);
@@ -72,28 +85,32 @@ void vectorread(FILE *fp, int line, vector *vect) {
 	val2[++i] = '\0';
 	vect->cmpnt.x = atof(val1);
 	vect->cmpnt.y = atof(val2);
+	//force times?
 }
 //reads nforces amount of forces into array of force forces[] from file fp
 void sforces(FILE *fp, const int nforces, force forces[]) {
-	fpos_t forcesloc = findstr("forces: %c\n	");
+	fpos_t forcesloc = findstr("forces: %s\n	");
 	char time[16], c;
 
 	for (int i = 0; i < nforces, i++) {
-		vectorread(fp, , forces[i]);
-		c = getc(fp);
-		s_gets(time, 16);
+		vectorread(fp, forcesloc, forces[i]);
+		c = getc(fp);		// move forward 1/skip space
+		while ((c = getc(fp)) != '\n' && i < 16)
+			temp[i++] = c;
 		forces[i].time = atoi(time);
+		forcesloc = ftell(fp);
 	}
 }
 //may be replaced by two vectorreads in main()
 void swindow(FILE *fp, vector window[2]) {
-	fpos_t winloc = findstr(fp, "window :");
+	fpos_t winloc = findstr(fp, "window: ");
 	for (int i = 0; i < 2; i++)
-		vectorread(fp, , window[i]);
+		vectorread(fp, winloc, window[i]);
 }
 //may be replaced by a vectorread in main()
 void stime(FILE *fp, vector *time) {
-
+	fpos_t tloc = findstr(fp, "time: ");
+	vectorread(fp, tloc, time);
 }
 //applies nforces amount of forces in array of force forces[] onto vectors in objects[], turning a force into a position using the newton's second law
 void applyforces(const unsigned int nforces, const force forces[], vector objects[], const double mass, const vector *time) {
@@ -111,6 +128,6 @@ void applyforces(const unsigned int nforces, const force forces[], vector object
 	}
 }
 //creates a visual 2d array of size window plotting the path of objects[] over time period time
-void display(const vector objects[], const vector window[2], const vector *time) {
+void writegraph(const vector objects[], const vector window[2], const vector *time) {
 
 }
